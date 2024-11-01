d9850d51146ec100444a6cb95d478295b0f3ca6e
lbeul <l.beul@outlook.de>
2024-11-01 19:16:17 +0100

    test: CommentInput functionality

---
+++ src/components/CommentSection/__tests__/CommentInput.test.tsx
@@ -0,0 +1,39 @@
+// src/components/Comment/__tests__/CommentInput.test.tsx
+import { render, screen, fireEvent } from "@testing-library/react";
+import CommentInput from "../CommentInput";
+import { vi, describe, it, expect } from "vitest";
+
+describe("CommentInput Component", () => {
+  const onSubmitMock = vi.fn();
+
+  it("renders the input form", () => {
+    render(<CommentInput onSubmit={onSubmitMock} />);
+    const textarea = screen.getByPlaceholderText("Write a comment...");
+    expect(textarea).toBeInTheDocument();
+  });
+
+  it("enables the submit button only when text is entered", () => {
+    render(<CommentInput onSubmit={onSubmitMock} />);
+    const textarea = screen.getByPlaceholderText("Write a comment...");
+    const submitButton = screen.queryByRole("button", { name: "Comment" });
+
+    expect(submitButton).not.toBeInTheDocument();
+
+    fireEvent.change(textarea, { target: { value: "Test input" } });
+    const submitButtonAfterInput = screen.getByRole("button", {
+      name: "Comment",
+    });
+    expect(submitButtonAfterInput).toBeInTheDocument();
+  });
+
+  it("calls onSubmit with the input content and clears the input on submit", () => {
+    render(<CommentInput onSubmit={onSubmitMock} />);
+    const textarea = screen.getByPlaceholderText("Write a comment...");
+
+    fireEvent.change(textarea, { target: { value: "New comment" } });
+    fireEvent.submit(screen.getByRole("button", { name: "Comment" }));
+
+    expect(onSubmitMock).toHaveBeenCalledWith("New comment");
+    expect(textarea).toHaveValue("");
+  });
+});
