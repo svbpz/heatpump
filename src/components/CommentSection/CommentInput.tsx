cacfc54715aff1b04f1b114923fc0ba2f7c387c9
lbeul <l.beul@outlook.de>
2024-11-01 18:00:43 +0100

    style: add tailwind styles to comment form

--- src/components/CommentSection/CommentInput.tsx
+++ src/components/CommentSection/CommentInput.tsx
@@ -20,11 +20,19 @@ const CommentInput = ({ onSubmit }: CommentInputProps) => {
   return (
     <form onSubmit={handleSubmit} className="flex flex-col my-4">
       <textarea
+        className="text-slate-900 p-2 rounded-lg resize-none h-20 border-2 border-slate-200 bg-transparent"
         placeholder="Write a comment..."
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      {isValidContent && <button type="submit">Comment</button>}
+      {isValidContent && (
+        <button
+          type="submit"
+          className="bg-slate-800 text-white py-2 px-4 rounded-lg mt-2 hover:bg-slate-950"
+        >
+          Comment
+        </button>
+      )}
     </form>
   );
 };
