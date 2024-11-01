1d94d8a8d8e4b9210f03abc55551c4564a967ec9
lbeul <l.beul@outlook.de>
2024-11-01 17:57:14 +0100

    style: highlight own comments and adjust nested comments styles

--- src/components/CommentSection/Comment.tsx
+++ src/components/CommentSection/Comment.tsx
@@ -3,21 +3,31 @@ import { CommentType } from "../../types";
 
 type CommentProps = {
   comment: CommentType;
+  nested?: boolean;
 };
 
-const Comment = ({ comment }: CommentProps) => {
+const Comment = ({ comment, nested = false }: CommentProps) => {
   const [showReplies, setShowReplies] = useState(false);
 
   const { author, date, content, replies } = comment;
   const hasReplies = replies.length > 0;
+  const isOwner = author === "You";
 
   const toggleReplies = () => setShowReplies((state) => !state);
 
+  const containerStyles = nested
+    ? `text-slate-900 py-2 pl-4 mb-4 border-l-2 hover:bg-slate-100 ${
+        isOwner ? "border-blue-500" : ""
+      }`
+    : "text-slate-900 p-4 mb-6 border-2 rounded-lg hover:bg-slate-50 ";
+
   return (
-    <article className="text-slate-900 p-4 mb-6 border-2 rounded-lg hover:bg-slate-50">
+    <article className={containerStyles}>
       <div className="flex items-center justify-between mb-2">
         <div className="flex flex-col">
-          <h3 className="font-semibold">{author}</h3>
+          <h3 className={`font-semibold ${isOwner ? "text-blue-700" : ""}`}>
+            {author}
+          </h3>
           <p className="text-slate-700 text-sm">{date}</p>
         </div>
       </div>
@@ -32,7 +42,7 @@ const Comment = ({ comment }: CommentProps) => {
       {showReplies && (
         <div className="ml-2 mt-4">
           {replies.map((reply) => (
-            <Comment key={reply.id} comment={reply} />
+            <Comment key={reply.id} comment={reply} nested />
           ))}
         </div>
       )}
