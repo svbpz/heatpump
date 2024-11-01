c0c54d87920e5234ce834a27f0fa17db57486e97
lbeul <l.beul@outlook.de>
2024-11-01 19:12:38 +0100

    test: add testing libs and localStorage mock function

---
+++ src/tests/setupTests.ts
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+
+const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+  return {
+    getItem(key: string) {
+      return store[key] || null;
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
